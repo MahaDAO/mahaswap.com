2884f297b3f01cc3d13f5285ccf695be16e9eb11
Steven Enamakel <enamakel@vezures.com>
2021-02-01 16:34:22 +0300

    changed to a red color

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -42,34 +42,34 @@ export function colors(darkMode: boolean): Colors {
 
     // text
     text1: darkMode ? '#FFFFFF' : '#000000',
-    text2: darkMode ? '#C3C5CB' : '#565A69',
-    text3: darkMode ? '#6C7284' : '#888D9B',
-    text4: darkMode ? '#565A69' : '#C3C5CB',
-    text5: darkMode ? '#2C2F36' : '#EDEEF2',
+    text2: darkMode ? '#cbc3c3' : '#565A69',
+    text3: darkMode ? '#846c6c' : '#888D9B',
+    text4: darkMode ? '#695656' : '#C3C5CB',
+    text5: darkMode ? '#362c2c' : '#EDEEF2',
 
     // backgrounds / greys
-    bg1: darkMode ? '#212429' : '#FFFFFF',
-    bg2: darkMode ? '#2C2F36' : '#F7F8FA',
-    bg3: darkMode ? '#40444F' : '#EDEEF2',
-    bg4: darkMode ? '#565A69' : '#CED0D9',
-    bg5: darkMode ? '#6C7284' : '#888D9B',
+    bg1: darkMode ? '#282521' : '#FFFFFF',
+    bg2: darkMode ? '#362c2c' : '#F7F8FA',
+    bg3: darkMode ? '#4f4040' : '#EDEEF2',
+    bg4: darkMode ? '#695956' : '#CED0D9',
+    bg5: darkMode ? '#846c6c' : '#888D9B',
 
     //specialty colors
     modalBG: darkMode ? 'rgba(0,0,0,.425)' : 'rgba(0,0,0,0.3)',
     advancedBG: darkMode ? 'rgba(0,0,0,0.1)' : 'rgba(255,255,255,0.6)',
 
     //primary colors
-    primary1: darkMode ? '#2172E5' : '#ff007a',
-    primary2: darkMode ? '#3680E7' : '#FF8CC3',
-    primary3: darkMode ? '#4D8FEA' : '#FF99C9',
-    primary4: darkMode ? '#376bad70' : '#F6DDE8',
-    primary5: darkMode ? '#153d6f70' : '#FDEAF1',
+    primary1: darkMode ? '#e22222' : '#ff007a',
+    primary2: darkMode ? '#e53737' : '#FF8CC3',
+    primary3: darkMode ? '#e84c4c' : '#FF99C9',
+    primary4: darkMode ? '#aa363670' : '#F6DDE8',
+    primary5: darkMode ? '#6d141470' : '#FDEAF1',
 
     // color text
-    primaryText1: darkMode ? '#6da8ff' : '#ff007a',
+    primaryText1: darkMode ? '#ff6d6d' : '#ff007a',
 
     // secondary colors
-    secondary1: darkMode ? '#2172E5' : '#ff007a',
+    secondary1: darkMode ? '#e22222' : '#ff007a',
     secondary2: darkMode ? '#17000b26' : '#F6DDE8',
     secondary3: darkMode ? '#17000b26' : '#FDEAF1',
 
@@ -194,7 +194,7 @@ body {
 }
 
  a {
-   color: ${colors(false).blue1}; 
+   color: ${colors(false).blue1};
  }
 
 * {
@@ -212,7 +212,7 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   font-feature-settings: 'ss01' on, 'ss02' on, 'cv01' on, 'cv03' on;
-  
+
 }
 `
 
